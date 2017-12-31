1ca8d97b40ed04b613129b2aab2dde12e94b3df6
bother7 <josephmcha@gmail.com>
2017-12-31 15:21:38 -0500

    added folders

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import reducer from './reducers/index'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {createLogger} from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const loggerMiddleware = createLogger()
+let store = createStore(reducer,
+  applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      loggerMiddleware // neat middleware that logs actions
+    ))
+
+ReactDOM.render(  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
